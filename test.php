064e9bddc1dece08d0989b2dc346bae0aa44f790
cfy <1002161225@qq.com>
2023-06-12 11:07:06 +0800

    !

--- test.php
+++ test.php
@@ -3,7 +3,7 @@ include 'vendor/autoload.php';
 
 // tlh .... ggggg  1 123
 
-// tlh .... ggg  1  123
+// tlh .... ggg  1  123  12312312
 $aha = new \CherryLu\CompanyWeChat\CompanyWeChat();
 
 // tlh ....  ggg1   123
