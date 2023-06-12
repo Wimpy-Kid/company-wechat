47ac7e117aaf15c7b153836973aa0b5d90e4843d
cfy <1002161225@qq.com>
2023-06-12 11:19:22 +0800

    ~

--- test.php
+++ test.php
@@ -1,11 +1,11 @@
 <?php
 include 'vendor/autoload.php';
 
-// tlh .... ggggg  1 123
+// tlh .... ggggg  1 123 灌灌灌灌灌
 
-// tlh .... ggg  1  123
+// tlh .... ggg  1  123   123123123
 $aha = new \CherryLu\CompanyWeChat\CompanyWeChat();
 
-// tlh ....  ggg1   123
+// tlh ....  ggg1   123 999999
 
 echo json_encode($aha->department->departmentList());
